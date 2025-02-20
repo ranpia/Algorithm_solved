81985fba2bdafe6041352ea53a058f0ac5b4e8d7
김정현 <163100604+ranpia@users.noreply.github.com>
2025-02-20 12:03:21 +0900

    [Silver V] Title: 분수찾기, Time: 236 ms, Memory: 19232 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int num = sc.nextInt();
+		sc.close();
+		int dir = 0; // 0: 오른쪽 1: 왼쪽아래 2: 아래 3: 오른쪽 위
+		int x = 0;
+		int y = 0;
+		int i = 1;
+
+		if (num > 1) {
+			while (i < num) {
+				switch (dir) {
+				case 0:
+					y++;
+					i++;
+					break;
+				case 1:
+					for (; y >= 1&&i<num;) {
+						x++;
+						y--;
+						i++;
+					}
+					break;
+				case 2:
+					x++;
+					i++;
+					break;
+				case 3:
+					for (; x >= 1&&i<num;) {
+						y++;
+						x--;
+						i++;
+					}
+					break;
+
+				}
+				dir = (dir + 1) % 4;
+			}
+		}
+		System.out.println((x + 1) + "/" + (y + 1));
+	}
+
+}
