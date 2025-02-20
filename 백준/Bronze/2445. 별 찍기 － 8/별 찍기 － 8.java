ff12ecb9bc5a19e5071fa91d7daacff173aca102
김정현 <163100604+ranpia@users.noreply.github.com>
2025-02-20 17:12:05 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 200 ms, Memory: 18300 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -4,35 +4,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
-		
-		for (int i=1; i<n+1; i++) {
-			for(int j=0; j<i; j++) {
-				System.out.print("*");
-			}
-			for(int j=0; j<2*n-2*i; j++) {
-				System.out.print(" ");
-			}
-			for(int j=0; j<i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-		
-		for (int i=n-1; i>0; i--) {
-			for(int j=0; j<i; j++) {
-				System.out.print("*");
-			}
-			for(int j=0; j<2*n-2*i; j++) {
-				System.out.print(" ");
-			}
-			for(int j=0; j<i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
+		sc.close();
+
+		for (int i = 1; i <= n; i++) {
+			String line = String.format("%s%s%s", "*".repeat(i), " ".repeat(2 * (n - i)), "*".repeat(i));
+			System.out.println(line);
 		}
 
+		for (int i = n - 1; i >= 1; i--) {
+			String line = String.format("%s%s%s", "*".repeat(i), " ".repeat(2 * (n - i)), "*".repeat(i));
+			System.out.println(line);
+		}
 	}
 
 }
