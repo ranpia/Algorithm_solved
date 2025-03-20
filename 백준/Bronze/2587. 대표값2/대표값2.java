782e5127a473bf7b707bd7ce2e7e996c1d28ca8d
김정현 <163100604+ranpia@users.noreply.github.com>
2025-03-20 14:12:50 +0900

    [Bronze II] Title: 대표값2, Time: 172 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int [] nums = new int[5];
+		int total = 0;
+		
+		for (int i=0; i<nums.length; i++) {
+			nums[i]= sc.nextInt();
+			total+=nums[i];
+		}
+		sc.close();
+		
+		Arrays.sort(nums);
+		System.out.println(total/nums.length);
+		System.out.println(nums[2]);
+	}
+
+}
