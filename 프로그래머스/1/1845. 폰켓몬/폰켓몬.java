ad87f361d8687ac51c1e8d02fdd1d23630befb4d
김정현 <163100604+ranpia@users.noreply.github.com>
2026-08-24 13:27:52 +0900

    [level 1] Title: 폰켓몬, Time: 3.54 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i: nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        
+        int answer = nums.length/2;
+
+        if (map.keySet().size()<answer){
+            answer = map.keySet().size();
+        }
+
+        return answer;
+    }
+}
